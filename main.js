b3a0b62d526d48ba9a035137901461550a19dc33
k3vwe <rukkimax@gmail.com>
2020-07-14 06:57:12 +0100

    calculator delete button function added

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 $(function(){
 
-
     var $screen = $('#screen');
+
+    function calculate(value){
+        
+    }
+
     $('span').on('click', function(e){
         
         var value = $(this).text();
+        if(value == 'del'|| value == '='){
+            value = '';
+        }
+        
         $screen.append(value);
 
-        if( $(this).attr('class') == 'default'){
-            if(value == 'AC'){
-                value = '';
-            }
-
-            if(($(this).attr('id')) === 'multi'){
-                value = $screen.text();
-                value = Math.abs(parseInt(value));
-            }
-
+        if(value == 'AC'){
+            value = '';
             $screen.html(value);
         }
 
-        console.log(eval(-.5-+9*8**5-9));
+        if(($(this).attr('id')) === 'multi'){
+            value = $screen.text();
+            value = Math.abs(parseInt(value));
+            $screen.html(value);
+        }
+
+        if($(this).text() == 'del'){
+            value = $screen.html();
+            $screen.html(value.substr(0, value.length-1));
+        }
+
+        if($(this).text() == '='){
+            let $numArr = Array.from($screen.html())
+            console.log(typeof($screen.html()));
+            console.log($numArr);
+            var dat = parseInt('102/3');
+            console.log(dat);
+            
+            
+        }
         
     });
 
